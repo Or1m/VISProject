c295271f39ff7a3f8103c0b2606a856e1ff7a1b0
Your Name <you@example.com>
2020-11-25 00:35:38 +0100

    Pridany CMDTest a otestovane pripojenie k DB

---
+++ CMDTest/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CMDTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataLayer.DatabaseConnection.Instance.Connect());
+
+            Console.ReadKey();
+        }
+    }
+}
