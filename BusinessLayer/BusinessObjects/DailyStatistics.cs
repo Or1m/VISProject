8c061cb26cd570ae827e9eacb02dc42051d56a71
Miroslav Kačeriak <k.z.w.212@gmail.com>
2020-12-19 21:47:52 +0100

    Vybrate daily stats z legacy

---
+++ BusinessLayer/BusinessObjects/DailyStatistics.cs
@@ -0,0 +1,25 @@
+﻿using BusinessLayer.BusinessObjects.Behaviour;
+using DTO;
+using System;
+
+namespace BusinessLayer.BusinessObjects
+{
+    public class DailyStatistics : Persistable<DailyStatisticsDTO>
+    {
+        public DateTime Date { get; set; }
+        public int NumberOfUserReviews { get; set; }
+
+        public int NumberOfReviewerReviews { get; set; }
+
+
+        public override DailyStatisticsDTO ToDTO()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return Date.Date + " " + NumberOfUserReviews + " " + NumberOfReviewerReviews;
+        }
+    }
+}
